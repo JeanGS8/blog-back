d352c4511df73a202a2f3975951f7042b1c85aee
JeanGS8 <jean118877@hotmail.com>
2024-05-21 13:13:04 -0300

    fix: vercel

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder } from '@nestjs/swagger';
 import { SwaggerModule } from '@nestjs/swagger/dist';
 import { AppModule } from './app.module';
+import * as env from "dotenv";
+
+env.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule); // o back-end inteiro está armazenado no app
@@ -10,7 +13,7 @@ async function bootstrap() {
   const config = new DocumentBuilder() // criamos um construtor de documentação, com a configuração do sistema
   .setTitle('Blog Pessoal') // Titulo
   .setDescription('Projeto Blog Pessoal') // descrição
-  .setContact('Generation Brasil', 'http://www.generationbrasil.online', 'generation@hotmail.com') // contatos do criador
+  .setContact('Jean Gomes', 'https://portfolio-jeangs8.vercel.app/', 'jean118877@hotmail.com') // contatos do criador
   .setVersion('1.0') // versão da aplicação
   .addBearerAuth() // modelo do token de seguraça do sistema
   .build(); // Ele vai pegar as configurações que passamos acima e irá retornar tudo para a config
@@ -19,7 +22,8 @@ async function bootstrap() {
 
   process.env.tz = "-03:00"; // utilizar o horário de brasília  
   app.useGlobalPipes(new ValidationPipe()); // estou dizendo que meu validation está configurado para rodar de forma global
-  app.enableCors(); // Habilitar que o computador receba requisições de fora
+  
+  app.enableCors();
 
   await app.listen(process.env.PORT || 4000);
 }
